99bd7a32261c08b0f14e1ecb13b49d1972127336
KimDoHyun <kdhqwe1030@naver.com>
2025-01-13 15:12:26 +0900

    init: 환경 구축

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.send('Hello TypeScript');
+});
+
+app.listen('3000', () => {
+  console.log('Listening on Port ::: ' + 3000);
+});
+
+export default app;
