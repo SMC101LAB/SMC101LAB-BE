3b7243ef9273591d0ea1ecb1781c571bd4c8e5e1
KimDoHyun <kdhqwe1030@naver.com>
2025-01-13 17:42:08 +0900

    init: mongodb 환경 구축

---
+++ src/config/database.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const connectDB = async (): Promise<void> => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@smc101lab.11f1n.mongodb.net/?retryWrites=true&w=majority&appName=SMC101LAB`
+    );
+    console.log('MongoDB Connected');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
