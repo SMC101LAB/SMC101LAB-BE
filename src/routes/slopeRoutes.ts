9c1e1751be88df6cdeb7f27ffb51c219936fef60
KimDoHyun <kdhqwe1030@naver.com>
2025-01-27 00:55:20 +0900

    feat: 급경사지 데이터 수정기능 api 구현

--- src/routes/slopeRoutes.ts
+++ src/routes/slopeRoutes.ts
@@ -1,25 +1,28 @@
 import { Router } from 'express';
 import { validateAuth } from '../controllers/auth';
-import { batchAddSlopeData } from '../controllers/slope/slopeAddBatch';
+import { batchAddSlopeData } from '../controllers/slope/slopeAddBatch'; //데이터 엑셀 추가
 import {
   findNearbySlopes,
   getAllSlopes,
-} from '../controllers/slope/slopeLookUp';
-import { getOutlierData } from '../controllers/slope/slopeOutlierData';
-import { deleteSlopes } from '../controllers/slope/slopeController';
-import { searchSlopes } from '../controllers/slope/slopeSearch';
+} from '../controllers/slope/slopeLookUp'; // 근처 데이터 조회, 전체 데이터 조회
+import { getOutlierData } from '../controllers/slope/slopeOutlierData'; // 이상값 찾기
+import {
+  deleteSlopes,
+  updateSlopes,
+} from '../controllers/slope/slopeController'; //데이터 삭제 및 수정
+import { searchSlopes } from '../controllers/slope/slopeSearch'; // 데이터 검색
 
 const router = Router();
 
 router.get('/nearby', findNearbySlopes as any);
 router.get('/search', searchSlopes as any);
-
 router.use(validateAuth as any); // 인증 미들웨어를 인증이 필요한 라우트 앞에 배치
 
 router.post('/batch', ...(batchAddSlopeData as any));
 router.get('/batch', getAllSlopes as any);
 router.get('/outlier', getOutlierData as any);
 router.delete('/delete', deleteSlopes as any);
+router.put('/update', updateSlopes as any);
 export default router;
 
 // POST /api/slopes          // 단일 데이터 추가
