f57f31ead9e8683fbb6321e74ba0174d8a63ed7b
KimDoHyun <kdhqwe1030@naver.com>
2025-04-03 17:41:11 +0900

    fix: 엑셀 다운로드시 안전점검일련변호 추가

--- src/controllers/slope/slopeLookUp.ts
+++ src/controllers/slope/slopeLookUp.ts
@@ -311,7 +311,7 @@ export const downloadSlopesExcel = async (
         붕괴위험지구지정일자: slope.collapseRisk?.designationDate || '',
         정비사업년도: slope.maintenanceProject?.year || '',
         정비사업유형코드: slope.maintenanceProject?.type || '',
-        안전점검일련번호: '', // 모델에 해당 필드가 없는 것 같습니다
+        안전점검일련번호: slope.inspections?.serialNumber || '',
         안전점검일자: slope.inspections?.date || '',
         안전점검결과코드: slope.inspections?.result || '',
         재해위험도평가일련번호: slope.disaster?.serialNumber || '',
